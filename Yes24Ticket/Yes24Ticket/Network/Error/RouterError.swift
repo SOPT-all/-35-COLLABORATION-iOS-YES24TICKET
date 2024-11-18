d1f71f5ba76c3d0e3bb5935381ed454e3f1b9a02
SungMinCho-Kor <rhxoeh0011@gmail.com>
2024-11-18 16:54:11 +0900

    feat: DTO, Error 및 추상 네트워크 레이어 구축 #9

---
+++ Yes24Ticket/Yes24Ticket/Network/Error/RouterError.swift
@@ -0,0 +1,14 @@
+//
+//  RouterError.swift
+//  Yes24Ticket
+//
+//  Created by 조성민 on 11/18/24.
+//
+
+enum RouterError: Error {
+    
+    case encoding
+    case invalidURL
+    case invalidRequest
+    
+}
