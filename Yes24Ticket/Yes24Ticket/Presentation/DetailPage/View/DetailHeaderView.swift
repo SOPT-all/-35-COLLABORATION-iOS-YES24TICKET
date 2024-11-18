d54caefe1d518f2d744d73d946fcd9f4d8244c00
0gonge <Challengef0802@gmail.com>
2024-11-19 04:22:02 +0900

    feat: detailHeaderView 네비게이션 arrow 구현 #13

---
+++ Yes24Ticket/Yes24Ticket/Presentation/DetailPage/View/DetailHeaderView.swift
@@ -0,0 +1,65 @@
+//
+//  DetailHeaderView.swift
+//  Yes24Ticket
+//
+//  Created by 송여경 on 11/19/24.
+//
+
+import UIKit
+
+import SnapKit
+import Then
+
+class DetailHeaderView: UITableViewHeaderFooterView {
+    
+    private lazy var containerView = UIView().then {
+        $0.backgroundColor = .white
+    }
+    
+    private lazy var backButton = UIButton().then {
+        $0.setImage(UIImage(resource: .icnArrowLeft24), for: .normal)
+        $0.addTarget(self, action: #selector(handleDismiss), for: .touchUpInside)
+    }
+    
+    override init(reuseIdentifier: String?) {
+        super.init(reuseIdentifier: reuseIdentifier)
+        setStyle()
+        setUI()
+        setLayout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setStyle() {
+        
+    }
+    
+    func setUI() {
+        contentView.addSubview(containerView)
+        containerView.addSubview(backButton)
+    }
+    
+    func setLayout() {
+        containerView.snp.makeConstraints {
+            $0.top.equalToSuperview().offset(47)
+            $0.width.equalToSuperview()
+            $0.height.equalTo(260)
+        }
+        
+        backButton.snp.makeConstraints {
+            $0.top.equalToSuperview().offset(8)
+            $0.leading.equalToSuperview().offset(10)
+            $0.width.height.equalTo(24)
+        }
+    }
+    
+    @objc func handleDismiss() {
+        
+    }
+}
+
+#Preview {
+    DetailHeaderView()
+}
