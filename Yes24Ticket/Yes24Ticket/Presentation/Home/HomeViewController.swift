0c7a23900b6264531d5d48f5fd693ca06134bfaf
lalaurel <ajinpark0518@naver.com>
2024-11-19 04:16:25 +0900

    fix: 불필요한 코드 삭제

--- Yes24Ticket/Yes24Ticket/Presentation/Home/HomeViewController.swift
+++ Yes24Ticket/Yes24Ticket/Presentation/Home/HomeViewController.swift
@@ -11,19 +11,8 @@ class HomeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemRed
-        
-        let footer = Footer()
-        footer.backgroundColor = .white
-        view.addSubview(footer)
-        
-        footer.translatesAutoresizingMaskIntoConstraints = false
-        NSLayoutConstraint.activate([
-            footer.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-            footer.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-            footer.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
-            footer.heightAnchor.constraint(equalToConstant: 300)
-        ])
+        view.backgroundColor = .systemGray
     }
+
 }
 
