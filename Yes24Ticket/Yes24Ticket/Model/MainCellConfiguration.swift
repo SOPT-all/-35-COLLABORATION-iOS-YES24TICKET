f4c2dea7cd415eb6785673f64fa68684e50786b7
SungMinCho-Kor <rhxoeh0011@gmail.com>
2024-11-20 03:30:26 +0900

    feat: 메인 Cell 구현 #19

---
+++ Yes24Ticket/Yes24Ticket/Model/MainCellConfiguration.swift
@@ -0,0 +1,42 @@
+//
+//  MainCellConfiguration.swift
+//  Yes24Ticket
+//
+//  Created by 조성민 on 11/19/24.
+//
+
+import UIKit
+
+struct MainCellConfiguration {
+    
+    let title: String
+    let area: String
+    let date: String
+    let image: UIImage
+    
+}
+
+extension MainCellConfiguration {
+    
+    static var mockData: [MainCellConfiguration] = [
+        .init(
+            title: "뮤지컬 시지프스1",
+            area: "예스24스테이지 1관",
+            date: "2024.12 - 2025.01",
+            image: .logoYes24Ios
+        ),
+        .init(
+            title: "뮤지컬 시지프스2",
+            area: "예스24스테이지 2관",
+            date: "2024.12 - 2025.02",
+            image: .logoYes24Ios
+        ),
+        .init(
+            title: "뮤지컬 시지프스3",
+            area: "예스24스테이지 3관",
+            date: "2024.12 - 2025.03",
+            image: .logoYes24Ios
+        )
+    ]
+    
+}
