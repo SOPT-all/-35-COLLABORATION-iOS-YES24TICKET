1926033c74c9cfd80d53ef7b10140d67f9408108
lalaurel <ajinpark0518@naver.com>
2024-11-21 02:44:13 +0900

    fix: 코드 정렬 #18

--- Yes24Ticket/Yes24Ticket/Presentation/TabBar/ClosedSearchBarView.swift
+++ Yes24Ticket/Yes24Ticket/Presentation/TabBar/ClosedSearchBarView.swift
@@ -11,35 +11,35 @@ import SnapKit
 import Then
 
 final class ClosedSearchBarView: UIView {
-
+    
     private let statusBarView = UIView().then {
         $0.backgroundColor = .black
     }
-
+    
     private let containerView = UIView().then {
         $0.backgroundColor = .black
     }
-
+    
     private let logoButton = UIButton().then {
         var configuration = UIButton.Configuration.plain()
         configuration.image = UIImage.logoYes24Ios
         $0.configuration = configuration
     }
-
+    
     private let searchButton = UIButton().then {
         $0.setImage(
-                .icSearchGray24,
-                for: .normal
-            )
+            .icSearchGray24,
+            for: .normal
+        )
     }
-
+    
     init() {
         super.init(frame: .zero)
         setStyle()
         setUI()
         setLayout()
     }
-
+    
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
@@ -54,26 +54,26 @@ final class ClosedSearchBarView: UIView {
         containerView.addSubview(logoButton)
         containerView.addSubview(searchButton)
     }
-
+    
     private func setLayout() {
         statusBarView.snp.makeConstraints {
             $0.top.leading.trailing.equalToSuperview()
             $0.height.equalTo(44)
         }
-
+        
         containerView.snp.makeConstraints {
             $0.top.equalTo(statusBarView.snp.bottom)
             $0.leading.trailing.equalToSuperview()
             $0.height.equalTo(61)
         }
-
+        
         logoButton.snp.makeConstraints {
             $0.leading.equalToSuperview().inset(14)
             $0.centerY.equalToSuperview()
             $0.width.equalTo(100)
             $0.height.equalTo(45)
         }
-
+        
         searchButton.snp.makeConstraints {
             $0.trailing.equalToSuperview().inset(14)
             $0.centerY.equalToSuperview()
