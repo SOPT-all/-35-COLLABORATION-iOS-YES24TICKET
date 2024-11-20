1e963c1f3e0e13f1e865abd94e3a3fdbac49e450
lalaurel <ajinpark0518@naver.com>
2024-11-21 02:01:30 +0900

    feat: 닫힌 검색바 구현 #18

--- Yes24Ticket/Yes24Ticket/Presentation/TabBar/ClosedSearchBarView.swift
+++ Yes24Ticket/Yes24Ticket/Presentation/TabBar/ClosedSearchBarView.swift
@@ -10,30 +10,66 @@ import SnapKit
 import Then
 
 class ClosedSearchBarView: UIView {
-    
-    private let searchButton = UIButton().then {
-        $0.setImage(.icSearchGray24, for: .normal)
+
+    private let statusBarView = UIView().then {
+        $0.backgroundColor = .black
     }
-    
+
+    private let containerView = UIView().then {
+        $0.backgroundColor = .black
+    }
+
+    private let logoButton = UIButton().then {
+        var configuration = UIButton.Configuration.plain()
+        configuration.image = UIImage(named: "logoYes24Ios")
+        $0.configuration = configuration
+    }
+
+    private let searchButton = UIButton().then {
+        $0.setImage(UIImage(named: "icSearchGray24"), for: .normal)
+    }
+
     init() {
         super.init(frame: .zero)
         setUI()
         setLayout()
+        backgroundColor = .black
     }
-    
+
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
+
     private func setUI() {
-        addSubview(searchButton)
+        addSubview(statusBarView)
+        addSubview(containerView)
+        containerView.addSubview(logoButton)
+        containerView.addSubview(searchButton)
     }
-    
+
     private func setLayout() {
+        statusBarView.snp.makeConstraints {
+            $0.top.leading.trailing.equalToSuperview()
+            $0.height.equalTo(44)
+        }
+
+        containerView.snp.makeConstraints {
+            $0.top.equalTo(statusBarView.snp.bottom)
+            $0.leading.trailing.equalToSuperview()
+            $0.height.equalTo(61)
+        }
+
+        logoButton.snp.makeConstraints {
+            $0.leading.equalToSuperview().inset(14)
+            $0.centerY.equalToSuperview()
+            $0.width.equalTo(100)
+            $0.height.equalTo(45)
+        }
+
         searchButton.snp.makeConstraints {
-            $0.width.height.equalTo(24)
             $0.trailing.equalToSuperview().inset(14)
             $0.centerY.equalToSuperview()
+            $0.width.height.equalTo(24)
         }
     }
 }
