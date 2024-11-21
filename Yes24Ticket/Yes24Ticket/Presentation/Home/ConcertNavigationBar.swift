2c068303cdfae6814dceb9f8ed11bd7d00661069
lalaurel <ajinpark0518@naver.com>
2024-11-22 03:30:23 +0900

    fix: 코드리뷰 반영 #29

--- Yes24Ticket/Yes24Ticket/Presentation/Home/ConcertNavigationBar.swift
+++ Yes24Ticket/Yes24Ticket/Presentation/Home/ConcertNavigationBar.swift
@@ -12,10 +12,6 @@ import Then
 
 final class ConcertNavigationBar: UIView {
     
-    private let bottomBorder = UIView().then {
-        $0.backgroundColor = .gray200
-    }
-    
     private let backButton = UIButton().then {
         $0.setImage(
             UIImage(resource: .icnArrowLeft24),
@@ -41,6 +37,7 @@ final class ConcertNavigationBar: UIView {
     override init(frame: CGRect) {
         super.init(frame: frame)
         setUI()
+        setStyle()
         setLayout()
     }
     
@@ -49,18 +46,16 @@ final class ConcertNavigationBar: UIView {
     }
     
     private func setUI() {
-        backgroundColor = .gray50
-        addSubview(bottomBorder)
         addSubview(backButton)
         addSubview(concertTitleLabel)
+        addBottomBorder(color: .gray200, height: 1)
+    }
+    
+    private func setStyle(){
+        backgroundColor = .gray50
     }
     
     private func setLayout() {
-        bottomBorder.snp.makeConstraints {
-            $0.bottom.leading.trailing.equalToSuperview()
-            $0.height.equalTo(1)
-        }
-        
         backButton.snp.makeConstraints {
             $0.top.equalToSuperview().offset(8)
             $0.leading.equalToSuperview().offset(10)
@@ -74,4 +69,16 @@ final class ConcertNavigationBar: UIView {
         }
     }
     
+}
+
+private extension UIView {
+    func addBottomBorder(color: UIColor, height: CGFloat) {
+        let border = UIView()
+        border.backgroundColor = color
+        addSubview(border)
+        border.snp.makeConstraints {
+            $0.bottom.leading.trailing.equalToSuperview()
+            $0.height.equalTo(height)
+        }
+    }
 }
