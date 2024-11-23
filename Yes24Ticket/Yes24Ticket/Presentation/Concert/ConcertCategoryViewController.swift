bd1f5084fad0e0a826d372c7f5e654d95473674d
lalaurel <ajinpark0518@naver.com>
2024-11-23 23:19:46 +0900

    feat: 콘서트 카테고리 뷰컨트롤러 구현 #37

---
+++ Yes24Ticket/Yes24Ticket/Presentation/Concert/ConcertCategoryViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ConcertCategoryViewController.swift
+//  Yes24Ticket
+//
+//  Created by 어진 on 11/22/24.
+//
+
+import UIKit
+
+final class ConcertCategoryViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let concertCategoryView = ConcertCategoryView()
+        view.addSubview(concertCategoryView)
+        
+        concertCategoryView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+    }
+}
+
+
