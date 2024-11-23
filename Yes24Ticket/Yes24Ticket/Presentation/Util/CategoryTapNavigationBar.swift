0b5f49f88f6c8f75d001c917ccb2157a7bc9e26b
lalaurel <ajinpark0518@naver.com>
2024-11-23 23:07:53 +0900

    feat: 콘서트 탭 컨트롤러 구현 #37

---
+++ Yes24Ticket/Yes24Ticket/Presentation/Util/CategoryTapNavigationBar.swift
@@ -0,0 +1,84 @@
+//
+//  CategoryTapNavigationBar.swift
+//  Yes24Ticket
+//
+//  Created by 어진 on 11/23/24.
+//
+
+import UIKit
+import SnapKit
+
+final class CategoryTapNavigationBar: UIView {
+    
+    private let recommendButton = UIButton().then {
+        $0.setTitle("추천공연", for: .normal)
+        $0.setTitleColor(.gray700, for: .normal)
+        $0.titleLabel?.font = .customFont(.button_b_13)
+        $0.isSelected = true
+        $0.backgroundColor = .white
+    }
+    
+    private let rankingButton = UIButton().then {
+        $0.setTitle("예매랭킹", for: .normal)
+        $0.setTitleColor(.gray700, for: .normal)
+        $0.titleLabel?.font = .customFont(.button_r_13)
+        $0.isSelected = false
+        $0.backgroundColor = .gray100
+    }
+    
+    private let listButton = UIButton().then {
+        $0.setTitle("전체리스트", for: .normal)
+        $0.setTitleColor(.gray700, for: .normal)
+        $0.titleLabel?.font = .customFont(.button_r_13)
+        $0.isSelected = false
+        $0.backgroundColor = .gray100
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupUI()
+        setupLayout()
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        recommendButton.addBorders(edges: [.top], color: .gray200, width: 1)
+        rankingButton.addBorders(edges: [.top, .bottom, .left], color: .gray200, width: 1)
+        listButton.addBorders(edges: [.top, .bottom, .left], color: .gray200, width: 1)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupUI() {
+        backgroundColor = .white
+        addSubview(recommendButton)
+        addSubview(rankingButton)
+        addSubview(listButton)
+    }
+    
+    private func setupLayout() {
+        recommendButton.snp.makeConstraints {
+            $0.leading.equalToSuperview()
+            $0.top.equalToSuperview()
+            $0.bottom.equalToSuperview()
+            $0.width.equalToSuperview().dividedBy(3)
+        }
+        
+        rankingButton.snp.makeConstraints {
+            $0.leading.equalTo(recommendButton.snp.trailing)
+            $0.top.equalToSuperview()
+            $0.bottom.equalToSuperview()
+            $0.width.equalToSuperview().dividedBy(3)
+        }
+        
+        listButton.snp.makeConstraints {
+            $0.leading.equalTo(rankingButton.snp.trailing)
+            $0.top.equalToSuperview()
+            $0.bottom.equalToSuperview()
+            $0.trailing.equalToSuperview()
+        }
+    }
+    
+}
