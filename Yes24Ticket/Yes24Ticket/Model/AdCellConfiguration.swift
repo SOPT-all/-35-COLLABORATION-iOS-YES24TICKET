b638465dacfcee75c876a9b084366b207808c122
SungMinCho-Kor <rhxoeh0011@gmail.com>
2024-11-26 02:54:54 +0900

    fix: Cell configuration에 id 추가 #41

--- Yes24Ticket/Yes24Ticket/Model/AdCellConfiguration.swift
+++ Yes24Ticket/Yes24Ticket/Model/AdCellConfiguration.swift
@@ -9,6 +9,7 @@ import UIKit
 
 struct AdCellConfiguration {
     
+    let id: Int
     let image: UIImage
     
 }
@@ -16,9 +17,9 @@ struct AdCellConfiguration {
 extension AdCellConfiguration {
     
     static let mockData: [AdCellConfiguration] = [
-        .init(image: .icSitBlue18),
-        .init(image: .icnArrayRed18I),
-        .init(image: .icnFillUpI)
+        .init(id: 0, image: .icSitBlue18),
+        .init(id: 1, image: .icnArrayRed18I),
+        .init(id: 2, image: .icnFillUpI)
     ]
     
 }
--- Yes24Ticket/Yes24Ticket/Model/TicketRankCellConfiguration.swift
+++ Yes24Ticket/Yes24Ticket/Model/TicketRankCellConfiguration.swift
@@ -9,6 +9,7 @@ import UIKit
 
 struct TicketRankCellConfiguration {
     
+    let id: Int
     let image: UIImage
     let rank: Int
     
@@ -17,13 +18,13 @@ struct TicketRankCellConfiguration {
 extension TicketRankCellConfiguration {
     
     static let mockData: [TicketRankCellConfiguration] = [
-        .init(image: .icSitBlue18, rank: 1),
-        .init(image: .icnX18, rank: 2),
-        .init(image: .icIShare36, rank: 3),
-        .init(image: .icSitMint18, rank: 4),
-        .init(image: .icSearchGray, rank: 5),
-        .init(image: .icArrowRightBlue16, rank: 6),
-        .init(image: .logoYes24Ios, rank: 7)
+        .init(id: 0,image: .icSitBlue18, rank: 1),
+        .init(id: 1,image: .icnX18, rank: 2),
+        .init(id: 2,image: .icIShare36, rank: 3),
+        .init(id: 3,image: .icSitMint18, rank: 4),
+        .init(id: 4,image: .icSearchGray, rank: 5),
+        .init(id: 5,image: .icArrowRightBlue16, rank: 6),
+        .init(id: 6,image: .logoYes24Ios, rank: 7)
     ]
     
 }
